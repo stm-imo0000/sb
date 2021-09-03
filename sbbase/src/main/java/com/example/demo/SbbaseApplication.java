261b231c440599473c119d5479042efddd4a0dd9
stm-imo0000 <masahiro.satsuma@gmail.com>
2021-09-04 05:09:14 +0900

    first commit

---
+++ sbbase/src/main/java/com/example/demo/SbbaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbaseApplication.class, args);
+	}
+
+}
